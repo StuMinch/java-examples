1a4bf6ac81005108ec5957b874b87964c65e3784
Stuart Minchington <stuart@Stuarts-MacBook-Pro.local>
2023-08-23 22:26:58 -0700

    Final copy

--- src/test/java/DriverAcquisition.java
+++ src/test/java/DriverAcquisition.java
@@ -1,9 +1,7 @@
 import io.appium.java_client.ios.IOSDriver;
-import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.By;
 import org.openqa.selenium.MutableCapabilities;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.Random;
@@ -22,18 +20,18 @@ public class DriverAcquisition {
         return uuidStr;
     }
 
-    public static void logging() throws IOException {
-        Logger logger = Logger.getLogger("");
-        logger.setLevel(Level.FINEST);
-        Arrays.stream(logger.getHandlers()).forEach(handler -> {
-            handler.setLevel(Level.FINEST);
-        });
-    }
-
     public static void main(String[] args) throws Exception {
 
         // Generate a random UUID
         String localSessionUuid = generateUuid();
+
+        // Setup Logger
+        // https://www.selenium.dev/documentation/webdriver/troubleshooting/logging/
+        Logger logger = Logger.getLogger("");
+        logger.setLevel(Level.FINE);
+        Arrays.stream(logger.getHandlers()).forEach(handler -> {
+            handler.setLevel(Level.FINE);
+        });
 
         // Create a map of capabilities
         MutableCapabilities caps = new MutableCapabilities();
@@ -48,8 +46,8 @@ public class DriverAcquisition {
         sauceOptions.setCapability("username", System.getenv("SAUCE_USERNAME"));
         sauceOptions.setCapability("accessKey", System.getenv("SAUCE_ACCESS_KEY"));
         sauceOptions.setCapability("uuid", localSessionUuid);
-        sauceOptions.setCapability("build", "Stuart M1 Tests");
-        sauceOptions.setCapability("name", "Reproducing Grid Driver Acquistion Error - Java Edition");
+        sauceOptions.setCapability("build", "Troubleshoot Grid Driver Acquistion Error");
+        sauceOptions.setCapability("name", "Increased Java Logging");
         caps.setCapability("sauce:options", sauceOptions);
 
         // Start the timer
@@ -57,36 +55,43 @@ public class DriverAcquisition {
 
         // Acquire driver
         URL url = new URL("https://ondemand.us-west-1.saucelabs.com:443/wd/hub");
-        IOSDriver driver = new IOSDriver(url, caps);
+        IOSDriver driver;
+        try {
+            driver = new IOSDriver(url, caps);
+            // Stop the timer
+            long tEnd = System.currentTimeMillis();
 
-        // Stop the timer
-        long tEnd = System.currentTimeMillis();
+            // Log troubleshooting info
+            logger.info("Successfully acquired Sauce Session in " + ((tEnd - tStart)/1000) + " seconds");
+            logger.info("Local UUID: " + localSessionUuid);
+            logger.info("Sauce UUID: " + driver.getSessionId());
+            logger.info("Sauce URL: https://app.saucelabs.com/tests/" + driver.getSessionId());
 
-        // Log results
-        Logger logger = Logger.getLogger("");
-        logger.setLevel(Level.FINEST);
-        logger.info("Successfully acquired Sauce Session in " + ((tEnd - tStart)/1000) + " seconds");
-        logger.info("Local UUID: " + localSessionUuid);
-        logger.info("Sauce UUID: " + driver.getSessionId());
-        logger.info("Sauce URL: https://app.saucelabs.com/tests/" + driver.getSessionId());
+            // Open the Sauce Demo website
+            driver.get("https://saucedemo.com");
 
-        // Open the Sauce Demo website
-        driver.get("https://saucedemo.com");
+            // Enter the username and password
+            driver.findElement(By.id("user-name")).sendKeys("standard_user");
+            driver.findElement(By.id("password")).sendKeys("secret_sauce");
 
-        // Enter the username and password
-        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+            // Click the login button
+            driver.findElement(By.id("login-button")).click();
 
-        // Click the login button
-        driver.findElement(By.id("login-button")).click();
+            // Assert that the title element is displayed
+            assert driver.findElement(By.className("title")).isDisplayed();
 
-        // Assert that the title element is displayed
-        assert driver.findElement(By.className("title")).isDisplayed();
+            // Set the job result to passed
+            driver.executeScript("sauce:job-result=passed");
 
-        // Set the job result to passed
-        driver.executeScript("sauce:job-result=passed");
+            // Quit the driver
+            driver.quit();
+        } catch (Exception e) {
+            // Capture the error
+            Logger exceptionLogger = Logger.getLogger("");
+            exceptionLogger.log(Level.SEVERE, "Error acquiring driver", e);
 
-        // Quit the driver
-        driver.quit();
+            // Exit the program
+            System.exit(1);
+        }
     }
 }
